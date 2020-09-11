215522da1a75c0058a31971c76d5eadf36f2762b
Vaibhav Prakash <vp1997dav@gmail.com>
2020-09-11 11:32:27 +0530

    News api added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+require('dotenv').config();
+const api=process.env.NEWS_API;
+
+const Newsapi = require('newsapi');
+const newsapi = new Newsapi(api);
+
+var news;
+newsapi.v2.topHeadlines({
+    category:'technology',
+    language:'en',
+    country:'us'
+}).then(res =>{
+    news=res;
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/news', function(req, res, next) {
+  res.render('news',{news:news,title:'Tech News'})
 });
 
 module.exports = router;
